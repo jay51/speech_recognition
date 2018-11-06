90a4ba4ae5e647e358c87283584336bffd1cbf77
jay51 <nenojaafar3@gmail.com>
2018-11-05 23:08:00 -0600

    i got the speech rec to start now i need to improve the UI and add more features

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const SpeechRecognition =
 	window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-
 recognition.interimResults = true;
+recognition.continuous = true;
+
+const container = document.querySelector(".container");
+const p = document.createElement("p");
+container.appendChild(p);
 
 recognition.addEventListener("result", function(e) {
-	console.log(e.results);
+	const transcript = Array.from(e.results)
+		.map(r => r[0])
+		.map(result => result.transcript)
+		.join(" ");
+
+	p.textContent = transcript;
+	console.log(transcript);
 });
 
 recognition.start();
